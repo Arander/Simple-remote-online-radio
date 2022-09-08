99db9acaa672a94b2f4c4bd6c00ace56e860a8f8
Arander <andres.bahamondes@gmail.com>
2022-09-08 15:39:25 -0400

    Add files via upload

--- 40norway.php
+++ 40norway.php
@@ -1,5 +1,19 @@
+<?php
 
-The return value of the `dir` command is 0
-<pre>array(0) {
-}
-</pre>
+    //Run `dir` command
+
+//    exec('cvlc --play-and-exit http://19993.live.streamtheworld.com/LOS40_SC --aout=alsa --alsa-audio-device="hw:CARD=Headphones,0" &', $output, $return);
+      exec('40norway > /dev/null 2> /dev/null &', $output, $return);
+    //Print the return status value
+
+    echo "The return value of the `dir` command is $return\n";
+
+    echo "<pre>";
+
+    //Print the command output
+
+    var_dump($output);
+
+    echo "</pre>";
+
+?>
--- radiooff.php
+++ radiooff.php
@@ -1,5 +1,20 @@
 
-The return value of the `dir` command is 0
-<pre>array(0) {
-}
-</pre>
+<?php
+
+    //Run `dir` command
+
+    exec("killstream > /dev/null 2> /dev/null &", $output, $return);
+
+    //Print the return status value
+
+    echo "The return value of the `dir` command is $return\n";
+
+    echo "<pre>";
+
+    //Print the command output
+
+    var_dump($output);
+
+    echo "</pre>";
+
+?>
