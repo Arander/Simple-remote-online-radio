13079cef620a8afea0d022b8f4d8c1e7c098182b
Arander <andres.bahamondes@gmail.com>
2023-10-04 13:14:22 -0300

    Shebangs and sudo -u added

--- 40norway.php
+++ 40norway.php
@@ -1,19 +1,10 @@
+#!/usr/bin/php
 <?php
-
-    //Run `dir` command
-
-//    exec('cvlc --play-and-exit http://19993.live.streamtheworld.com/LOS40_SC --aout=alsa --alsa-audio-device="hw:CARD=Headphones,0" &', $output, $return);
-      exec('40norway > /dev/null 2> /dev/null &', $output, $return);
-    //Print the return status value
-
-    echo "The return value of the `dir` command is $return\n";
-
-    echo "<pre>";
-
-    //Print the command output
-
-    var_dump($output);
-
-    echo "</pre>";
-
+exec('sudo -u www-data 40norway > /dev/null 2> /dev/null &', $output, $return);
+//Print the return status value
+echo "The return value of the `dir` command is $return\n";
+echo "<pre>";
+//Print the command output
+var_dump($output);
+echo "</pre>";
 ?>
